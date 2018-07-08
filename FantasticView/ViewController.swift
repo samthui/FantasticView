40787f23e1361752a081106c47f2f3e4cc9e7483
Ngoc T P Nguyen <samthui7@gmail.com>
2018-07-08 11:30:17 +0700

    FantasticView

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Smisy on 7/8/18.
+//  Copyright © 2018 Smisy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
